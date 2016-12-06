e5d3425469bf1d80b198deaf34ff07a04f9206e9
Deniss Makarenkov <makdeniss@gmail.com>
2016-12-06 15:50:15 +0200

    another service file to be injected into a home service.

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -2,24 +2,28 @@
     'use strict';
 
     angular.module('fuelComparatorApp').controller('homeController', homeController);
-    homeController.$inject = ["$scope", "$http", "$window", "$q", "sayHelloService", "someService"];
+    homeController.$inject = ["$scope", "$http", "$window", "$q", "sayHelloService", "someService", "flagMainService"];
 
-    function homeController($scope, $http, $window, $q, sayHelloService, someService) {
+    function homeController($scope, $http, $window, $q, sayHelloService, someService, flagMainService) {
         const vm = this;
         vm.fuelComparatorService = sayHelloService;
         vm.fuelComparatorService = someService;
+        vm.fuelComparatorService = flagMainService;
 
         sayHelloService.sayHi();
         sayHelloService.sayBye();
         someService.someServiceMessage();
+        flagMainService.whichFlag();
 
         vm.hello = sayHelloService.helloMessage.Message;
         vm.bye = sayHelloService.byeMessage.Message;
         vm.someMessage = someService.someMessageObject.someMessageString;
+        vm.whichFlag = flagMainService.flag.Message;
 
         console.log(vm.hello);
         console.log(vm.bye);
         console.log(vm.someMessage);
+        console.log(vm.whichFlag);
         return vm;
     }
 })();
